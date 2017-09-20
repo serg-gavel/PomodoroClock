e1512fd32545eba718c7105b62a4bd154c581251
Serg <serg-gavel@users.noreply.github.com>
2017-09-20 15:38:38 +0300

    Repository updated 20.09.2017

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,109 @@
 $(document).ready(function () {
-    breakCountLength();
-    sessionCountLength();
+    $('#reset-button').hide();
+    resetBtn();
+    // var start = $('#start');
+    var beep = $('#beep')[0];
+    var btnSound = $('#sound')[0];
+    var breakNum = parseInt($('#break').html());
+    var sessionNum = parseInt($('#session').html());
+    var counter = $('#counter').html();
+    // var reset = $('#reset-button');
+    var timerStatus = $('.timer-status>h6');
+
+    inGodWeTrust();
+
+    //start timer function
+    function inGodWeTrust() {
+        $('#start-timer').on('click', function () {
+            timerStatus.text('Started');
+            $('.plus, .minus').hide();
+            $('#break, #session').css('padding-left','15px');
+
+            var timerCounter = setInterval(sessionTimer, 10);
+            btnSound.play();
+            sessionNum*=60;
+            breakNum*=60;
+            function sessionTimer() {
+                $('#start-timer').off('click');
+                sessionNum-=1;
+                if(sessionNum===0){
+                    beep.play();
+                    clearInterval(timerCounter);
+                    timerStatus.text('Finished');
+                    var startBreakTimer = setInterval(breakTimer, 10);
+                }
+                if(sessionNum%60>=10){
+                    $('#counter').html(Math.floor(sessionNum/60)+':'+sessionNum%60);
+                }else{
+                    $('#counter').html(Math.floor(sessionNum/60)+':'+'0'+sessionNum%60);
+                }
+                console.log(sessionNum);
+
+                function breakTimer() {
+                    timerStatus.text('Break started');
+                    $('.timer-body').css('background','#00b04a').css('border','10px solid #007d35');
+                    breakNum-=1;
+                    if(breakNum===0){
+                        clearInterval(startBreakTimer);
+                        timerStatus.text('Break finished');
+                        $('#reset-button').show();
+                        beep.play();
+                    }
+                    if(breakNum%60>=10){
+                        $('#counter').html(Math.floor(breakNum/60)+':'+breakNum%60);
+                    }else{
+                        $('#counter').html(Math.floor(breakNum/60)+':'+'0'+breakNum%60);
+                    }
+                    console.log(breakNum);
+                }
+            }
+        });
+    }
+//plus-minus function
+    $('#session-minus').click(function () {
+        if (sessionNum>1){
+            sessionNum -=1;
+            clearSession();
+        }
+    });
+    $('#session-plus').click(function () {
+            sessionNum +=1;
+            clearSession();
+
+    });
+    $('#break-minus').click(function () {
+        if (breakNum>1){
+            breakNum -=1;
+            clearBreak();
+        }
+    });
+    $('#break-plus').click(function () {
+            breakNum +=1;
+            clearBreak();
+    });
+   function resetBtn() {
+       var reset = $('#reset-button');
+       reset.click(function () {
+           inGodWeTrust();
+           $('.timer-body').css('background','#a83e38').css('border','10px solid #82302c');
+           timerStatus.text('Click me!');
+           $('.plus, .minus').show();
+           $('#counter').html(counter=25);
+           $('#break').html(breakNum=5).css('padding-left','0px');
+           $('#session').html(sessionNum=25).css('padding-left','0px');
+           $('#reset-button').hide();
+       });
+   }
+    //additional functions
+    function clearSession() {
+        $('#session').html(sessionNum);
+        timerStatus.text('Session');
+        $('#counter').html(sessionNum);
+    }
+    function clearBreak() {
+        $('#break').html(breakNum);
+        timerStatus.text('Break');
+        $('#counter').html(breakNum);
+    }
+
 });
-
-
-var breakCounter = $('.break-counter'),
-    sessionCounter = $('.session-counter'),
-    breakLength = $('.break-length'),
-    sessionLength = $('.session-length'),
-    breakMinus = $('.break>.inner>.minus'),
-    breakPlus = $('.break>.inner>.plus'),
-    sessionMinus = $('.session>.inner>.minus'),
-    sessionPlus = $('.session>.inner>.plus'),
-    statusTitle = $('.timer-status>h6');
-
-
-function breakCountLength() {
-    $(breakPlus).click( function () {
-        statusTitle.text('');
-        statusTitle.text('Break');
-        sessionLength.text('');
-        var i = +breakCounter.text() + 1;
-        breakCounter.text(i) && breakLength.text(i);
-    });
-    $(breakMinus).click( function () {
-        statusTitle.text('');
-        statusTitle.text('Break');
-        sessionLength.text('');
-        var i = breakCounter.text() -1;
-        if(i<=0){
-            i =0;
-        }else{
-            breakCounter.text(i) && breakLength.text(i);
-
-        }
-    });
-}
-function sessionCountLength() {
-    $(sessionPlus).click( function () {
-        statusTitle.text('');
-        statusTitle.text('Session');
-        breakLength.text('');
-        var i = +sessionCounter.text() + 1;
-        sessionCounter.text(i) && sessionLength.text(i);
-    });
-    $(sessionMinus).click( function () {
-        statusTitle.text('');
-        statusTitle.text('Session');
-        breakLength.text('');
-        var i = sessionCounter.text() -1;
-        if(i<=0){
-            i =0;
-        }else{
-            sessionCounter.text(i) && sessionLength.text(i);
-
-        }
-    });
-}
-
-
